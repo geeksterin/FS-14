b56373a01c63c2682db2a915b63a89b9717bf0d8
jitmanew-tyagi <jitmanew@gmail.com>
2023-06-13 19:57:29 +0530

    Seventh class

--- Main.java
+++ Main.java
@@ -1,30 +1,9 @@
 import java.util.*;
 public class Main{ 
     public static void main(String[] args) {
-        System.out.println(!(5 < 7)); //f
-        System.out.println(!(7 <= (3 / 4))); //t
-        System.out.println(!false); //t
-        System.out.println((!(0 == (3 / 4.0)))); //t
-        System.out.println("!false"); // !false
-    }
-}
-
-x, y, z
-
-if(x >= 20) {
-    if(y >= 100) {
-        
-    } else if(y >= 50) {
-
-    } else {
-
-    }
-} else {
-    if(y >= 100) {
-
-    } else if(y >= 50) {
-
-    } else{
-
+        // System.out.println(!(5 < 7)); //f
+        // System.out.println(!(7 <= (3 / 4))); //t
+        // System.out.println(!false); //t
+        // System.out.println((!(0 == (3 / 4.0)))); //t
     }
 }
