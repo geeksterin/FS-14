e6d62bb99741abcfa4c6de9f5099760df23140bc
jitmanew-tyagi <jitmanew@gmail.com>
2023-06-27 16:23:28 +0530

    23 June 2023

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
+import java.io.*;
 import java.util.*;
-public class Main{ 
-    public static void main(String[] args) {
-        // System.out.println(!(5 < 7)); //f
-        // System.out.println(!(7 <= (3 / 4))); //t
-        // System.out.println(!false); //t
-        // System.out.println((!(0 == (3 / 4.0)))); //t
+
+public class Main {
+
+    public static void printTable(int n) {
+        for(int i = 1; i <= 10; i ++) {
+            System.out.println(n + " X " + i + " = " + (n * i));
+        }
+        System.out.println("***************************");
     }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        printTable(scn.nextInt());
+    }
+    
+    public static int getProfit(int CP, int SP, int units) {
+        int profit = (SP - CP) * units - 100;
+        return profit;
+    } 
+
+    
 }
