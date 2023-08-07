51332882941aa22b5af987b2be598e1237ccab54
jitmanew-tyagi <jitmanew@gmail.com>
2023-08-07 15:32:38 +0530

    Module Split

---
+++ M1/Arrays.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        incValue(a);
+        System.out.println(a);
+    }
+
+    public static void incValue(int a) {
+        a = a + 10;
+    }
+
+    for(int i = 1; i < n; i *= 2) {
+        System.out.println(i);
+    }
+
+    for(int i = 0; i <= n; i *= 10) {
+        // some statements of constant time
+        // addition
+        // mathematical staments
+        // printing statements O(log10(n))
+        // logical
+        // return 
+    }
+
+int[] arr = {1, 2, 3, 4 ,5}
+System.out.println(arr[7]);
+
+
+    n = 10000000;
+    n += 5; // O(1)
+
+    while(n > 0) {
+        System.out.println(n);
+        n -= 10;
+    } // O(n)
+
+    arr[2]
+}
+
+
+a = 10000;
+b = 5000;
+a * b;
+
